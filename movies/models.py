dcbc7c73181b0279d295ab7e52f88050a2285734
David <roberto@localhost.localdomain>
2019-10-18 15:03:11 -0300

    LAB 2 FSDI 12

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+"""
+    Charfield
+    Int
+    Float
+    Boolean
+"""
+
+class Genre(models.Model):
+    name = models.CharField(max_length=250)
+
+
+    def __str__(self):
+        return self.name
+
+class Movie(models.Model):
+    title = models.CharField(max_length=250)
+    release_year = models.IntegerField()
+    in_stock = models.IntegerField()
+    price = models.FloatField()
+    duration_min = models.IntegerField()
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
--- movies/urls.py
+++ movies/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('welcome', views.welcome, name="welcome"),
     path('myname', views.myname, name="myname"),
     path('hello', views.index, name="hello"),
+    path('', views.index, name="root"),
+    path('index', views.index, name="index"),
+
 
 ]
